1939d8845700283ed38b6da9240707cc2c25eb74
dario-cfpt <dario.gng@eduge.ch>
2019-11-26 10:11:09 +0100

    Init node dependencies and typescript

---
+++ server/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    // Allow client to receive the data
+    // from : https://enable-cors.org/server_expressjs.html
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+//# sourceMappingURL=index.js.map
